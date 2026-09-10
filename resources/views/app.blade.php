89c05ba36b9d1c2648fecbae050040198d67cc83
Your Real Name <your-email@example.com>
2026-09-10 17:01:21 +0530

    Support local dev HTTP cookies and disable CSP upgrade-insecure-requests on non-production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @production
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        @endproduction
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
